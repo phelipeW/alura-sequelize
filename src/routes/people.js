7887e40b481e4645519e14c3a558bacfc6b6e011
phelipeW <phelipe.william@hotmail.com>
2021-04-28 14:04:37 -0300

    feat: crud people

--- src/routes/people.js
+++ src/routes/people.js
@@ -4,5 +4,9 @@ const PeopleController = require('../controllers/PeopleController')
 const router = Router()
 
 router.get('/people', PeopleController.index)
+router.get('/people/:id', PeopleController.findById)
+router.post('/people', PeopleController.create)
+router.put('/people/:id', PeopleController.update)
+router.delete('/people/:id', PeopleController.delete)
 
 module.exports = router
